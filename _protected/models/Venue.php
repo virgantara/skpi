55502f6923241269e64b9e07df920ac6e97e6a4c
virgantara <oddy@unida.gontor.ac.id>
2021-06-21 14:39:29 +0700

    + venue

---
+++ _protected/models/Venue.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "erp_venue".
+ *
+ * @property int $id
+ * @property string $nama
+ * @property int|null $kapasitas
+ */
+class Venue extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'erp_venue';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nama'], 'required'],
+            [['kapasitas'], 'integer'],
+            [['nama'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nama' => 'Nama',
+            'kapasitas' => 'Kapasitas',
+        ];
+    }
+}
