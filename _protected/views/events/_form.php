79a2e185109ac1b50db2500aaa3f8f1e756f3d8d
virgantara <oddy@unida.gontor.ac.id>
2021-06-21 14:42:50 +0700

    fix

--- _protected/views/events/_form.php
+++ _protected/views/events/_form.php
@@ -50,7 +50,7 @@ EventAsset::register($this);
             <div class="control-group">
                 <label class="control-label" for="inputPatient">Venue/Lokasi Acara:</label>
                 <div class="field desc">
-                    <?=Html::dropDownList('venue','',ArrayHelper::map(\app\models\Venue::find()->all(),'id','nama'),['id'=>'venue','class'=>'form-control','prompt'=>'-Pilih Venue/Lokasi Acara-']);?>
+                    <?=Html::dropDownList('venue','',ArrayHelper::map(\app\models\Venue::find()->all(),'nama','nama'),['id'=>'venue','class'=>'form-control','prompt'=>'-Pilih Venue/Lokasi Acara-']);?>
                    
                 </div>
             </div>
@@ -122,7 +122,7 @@ EventAsset::register($this);
             <div class="control-group">
                 <label class="control-label" for="inputPatient">Venue/Lokasi Acara:</label>
                 <div class="field desc">
-                     <?=Html::dropDownList('venue_edit','',ArrayHelper::map(\app\models\Venue::find()->all(),'id','nama'),['id'=>'venue_edit','class'=>'form-control','prompt'=>'-Pilih Venue/Lokasi Acara-']);?>
+                     <?=Html::dropDownList('venue_edit','',ArrayHelper::map(\app\models\Venue::find()->all(),'nama','nama'),['id'=>'venue_edit','class'=>'form-control','prompt'=>'-Pilih Venue/Lokasi Acara-']);?>
                    
                 </div>
             </div>
