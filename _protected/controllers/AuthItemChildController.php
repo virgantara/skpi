e6902af8a55f18369ef50650dd7579401c0c46f5
nashehannafii <nashehannafii@gmail.com>
2022-09-08 13:36:53 +0700

    [Nasheh] + Auth Item & Auth Item Child Menu

---
+++ _protected/controllers/AuthItemChildController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\AuthItemChild;
+use app\models\AuthItemChildSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * AuthItemChildController implements the CRUD actions for AuthItemChild model.
+ */
+class AuthItemChildController extends Controller
+{
+    /**
+     * @inheritDoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Lists all AuthItemChild models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new AuthItemChildSearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single AuthItemChild model.
+     * @param string $parent Parent
+     * @param string $child Child
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($parent, $child)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($parent, $child),
+        ]);
+    }
+
+    /**
+     * Creates a new AuthItemChild model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new AuthItemChild();
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post()) && $model->save()) {
+                return $this->redirect(['view', 'parent' => $model->parent, 'child' => $model->child]);
+            }
+        } else {
+            $model->loadDefaultValues();
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing AuthItemChild model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $parent Parent
+     * @param string $child Child
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($parent, $child)
+    {
+        $model = $this->findModel($parent, $child);
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'parent' => $model->parent, 'child' => $model->child]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing AuthItemChild model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $parent Parent
+     * @param string $child Child
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($parent, $child)
+    {
+        $this->findModel($parent, $child)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the AuthItemChild model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $parent Parent
+     * @param string $child Child
+     * @return AuthItemChild the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($parent, $child)
+    {
+        if (($model = AuthItemChild::findOne(['parent' => $parent, 'child' => $child])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
